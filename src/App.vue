2883ee761014682cb611092ba1ab820f1cd91aeb
Lyuuu <lllyuwork@163.com>
2023-12-18 19:52:29 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <my-button>我是自定义按钮</my-button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import MyKit from '../packages';
+
+createApp(App).use(MyKit).mount('#app')
+
